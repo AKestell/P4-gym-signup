8e74f81972d06b65051e802686fc8452514b51b2
Adam Kestell <a.kestell89@hotmail.com>
2024-05-27 20:53:15 +0000

    Configured Secret Key

--- gymtimes/settings.py
+++ gymtimes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ouotwd$n_3)!p5hu11kdh$ej9elaaqs7=1#hes_l(i^c_df)nw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
