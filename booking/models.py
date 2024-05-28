e7821d38bae2d2bc598dda2f441dd3758a4c1e1b
Adam Kestell <a.kestell89@hotmail.com>
2024-05-28 19:54:46 +0000

    Added the GymClass model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class GymClass(models.Model):
+    DAYS = [
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),
+        (7, 'Sunday')
+    ]
+    
+    name = models.CharField(max_length=100)
+    day_of_week = models.IntegerField(choices=DAYS)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    instructor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} on {self.get_day_of_week_display()} from {self.start_time} to {self.end_time}"
